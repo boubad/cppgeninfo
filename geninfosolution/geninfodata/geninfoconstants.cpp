99b801158e43817fc5207e91c44297298421fd17
Boubacar Diarra <boubadiarra@gmail.com>
2015-10-08 00:30:13 +0200

    work

---
+++ geninfosolution/geninfodata/geninfoconstants.cpp
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+#include "genninfoconstants.h"
+namespace geninfo {
+	///////////////////////////////////////////
+	const string_t GenInfoConstants::QUESTION_STRING(U("?"));
+	const string_t GenInfoConstants::AND_STRING(U("&"));
+	const string_t GenInfoConstants::QUOTE_STRING(U("\""));
+	//
+	const string_t GenInfoConstants::COMMITTED_UPDATE_SEQ(U("committed_update_seq"));
+	const string_t GenInfoConstants::COMPACT_RUNNING(U("compact_running"));
+	const string_t GenInfoConstants::DB_NAME(U("db_name"));
+	const string_t GenInfoConstants::DISK_FORMAT_VERSION(U("disk_format_version"));
+	const string_t GenInfoConstants::DATA_SIZE(U("data_size"));
+	const string_t GenInfoConstants::DISK_SIZE(U("disk_size"));
+	const string_t GenInfoConstants::DOC_COUNT(U("doc_count"));
+	const string_t GenInfoConstants::DOC_DEL_COUNT(U("doc_del_count"));
+	const string_t GenInfoConstants::INSTANCE_START_TIME(U("instance_start_time"));
+	const string_t GenInfoConstants::PURGE_SEQ(U("purge_seq"));
+	const string_t GenInfoConstants::UPDATE_SEQ(U("update_seq"));
+	/////////////////////////////////////////////////
+	const string_t GenInfoConstants::TYPE(U("type"));
+	const string_t GenInfoConstants::ID(U("_id"));
+	const string_t GenInfoConstants::REV(U("_rev"));
+	const string_t GenInfoConstants::STATUS(U("status"));
+	const string_t GenInfoConstants::DESCRIPTION(U("description"));
+	///////////////////////////////////////////////////
+}// namespace geninfo
---
+++ geninfosolution/geninfodata/genninfoconstants.h
@@ -0,0 +1,60 @@
+#pragma once
+#ifndef __GENINFOCONSTANTS_H
+#define __GENINFOCONSTANTS_H
+////////////////////////////////
+#include "gendefs.h"
+///////////////////////////////
+namespace geninfo {
+	////////////////////////////////
+	class GenInfoConstants
+	{
+	public:
+		//
+		static const string_t QUESTION_STRING;
+		static const string_t AND_STRING;
+		static const string_t QUOTE_STRING;
+		//
+		static const string_t COMMITTED_UPDATE_SEQ;
+		static const string_t COMPACT_RUNNING;
+		static const string_t DB_NAME;
+		static const string_t DISK_FORMAT_VERSION;
+		static const string_t DATA_SIZE;
+		static const string_t DISK_SIZE;
+		static const string_t DOC_COUNT;
+		static const string_t DOC_DEL_COUNT;
+		static const string_t INSTANCE_START_TIME;
+		static const string_t PURGE_SEQ;
+		static const string_t UPDATE_SEQ;
+		//
+		static const string_t TYPE;
+		static const string_t ID;
+		static const string_t REV;
+		static const string_t STATUS;
+		static const string_t DESCRIPTION;
+		static const string_t SIGLE;
+		static const string_t NAME;
+		static const string_t ORDER;
+		static const string_t GENRE;
+		static const string_t CHILDRENIDS;
+		static const string_t STARTDATE;
+		static const string_t ENDDATE;
+		static const string_t MODULE;
+		static const string_t COEFFICIENT;
+		static const string_t ECS;
+		static const string_t DEPARTEMENTID;
+		static const string_t UNITEID;
+		static const string_t ANNEEID;
+		static const string_t MATIEREID;
+		static const string_t SEMESTREID;
+		static const string_t GROUPEID;
+		static const string_t ADMINISTRATORID;
+		static const string_t ENSEIGNANTID;
+		static const string_t ETUDIANTID;
+		static const string_t ENSEIGNANTAFFECTATIONID;
+		static const string_t ETUDIANTAFFECTATIONID;
+		static const string_t GROUPEEVENTID;
+		static const string_t DEPARTEMENTSIDS;
+	};
+}// namespace geninfo
+//////////////////////////
+#endif // __GENINFOCONSTANTS_H
